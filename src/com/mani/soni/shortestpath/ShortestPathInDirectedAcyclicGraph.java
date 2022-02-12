529e3d41a390fce1371bfa3482c3fa4b00ed6a53
Manish Kumar <manish.br.kumar@oracle.com>
2022-02-12 20:29:07 +0530

    Shortest Path in Directed Acyclic Graph

--- src/com/mani/soni/shortestpath/ShortestPathInDirectedAcyclicGraph.java
+++ src/com/mani/soni/shortestpath/ShortestPathInDirectedAcyclicGraph.java
@@ -5,6 +5,14 @@ import java.util.*;
 /**
  * We can use topological sort to find the edges which we have to
  * follow.
+ * Algorithm:
+ * Step 1) dist[v] = {INF, INF, ..., INF}
+ * Step 2) dist[source] = 0;
+ * Step 3) Find a topological sort of the graph.
+ * Step 4) For every vertex u in the topological sort
+ *        a) For every adjacent v of u:
+ *             If dist[v] > dist[u] + weight(u , v)
+ *                 dist[v] = dist[u] + weight(u , v)
  */
 public class ShortestPathInDirectedAcyclicGraph {
 
@@ -27,17 +35,22 @@ public class ShortestPathInDirectedAcyclicGraph {
             put(3, new ArrayList<>());
             put(0, List.of(new AdjacencyNode(1, 2), new AdjacencyNode(4, 1)));
         }};
-
+//        Map<Integer, List<AdjacencyNode>> graph = new HashMap<>() {{
+//            put(2, List.of(new AdjacencyNode(3, 4)));
+//            put(1, List.of(new AdjacencyNode(2, 3), new AdjacencyNode(3, 2)));
+//            put(3, new ArrayList<>());
+//            put(0, List.of(new AdjacencyNode(1, 1)));
+//        }};
         findShortestPath(graph, 0);
     }
 
     private static void findShortestPath(Map<Integer, List<AdjacencyNode>> graph, int i) {
         // find topological sort
-        topologicalSort(graph);
+        topologicalSort(graph, i);
 
     }
 
-    private static void topologicalSort(Map<Integer, List<AdjacencyNode>> graph) {
+    private static void topologicalSort(Map<Integer, List<AdjacencyNode>> graph, Integer starting) {
 
         Stack<Integer> stack = new Stack<>();
 
@@ -49,20 +62,21 @@ public class ShortestPathInDirectedAcyclicGraph {
             }
         }
 
-        int starting = 0;
         computeShortestDistance(graph, stack, starting);
     }
 
     private static void computeShortestDistance(Map<Integer, List<AdjacencyNode>> graph, Stack<Integer> stack, Integer starting) {
         Integer[] distance = new Integer[graph.size()];
-        Arrays.fill(distance, Integer.MAX_VALUE);
+        Arrays.fill(distance, 100000);
         distance[starting] = 0;
 
         while(!stack.isEmpty()) {
             Integer vertex = stack.pop();
             // distance[vertex]
             for (AdjacencyNode node: graph.get(vertex)) {
-                distance[node.vertex] = Math.min(distance[node.vertex], distance[vertex] + node.weight);
+                if(distance[node.vertex] > distance[vertex] + node.weight) {
+                    distance[node.vertex] = distance[vertex] + node.weight;
+                }
             }
         }
 
